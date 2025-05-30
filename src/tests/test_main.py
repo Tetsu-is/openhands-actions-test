f61265baea148a8dc66d07df712fbfb694eec12b
Tetsu-is <ca02481@cartahd.com>
2025-05-30 15:46:17 +0900

    fix test

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -9,17 +9,17 @@ def setup_function():
     Item._item_list = []
 
 def test_create_item():
-    response = client.post("/items/", json={"name": "テストアイテム"})
+    response = client.post("/api/items/", json={"name": "テストアイテム"})
     assert response.status_code == 200
     assert response.json() == {"message": "Item added", "item": "テストアイテム"}
 
 def test_read_items():
     # 最初にアイテムを追加
-    client.post("/items/", json={"name": "テストアイテム1"})
-    client.post("/items/", json={"name": "テストアイテム2"})
+    client.post("/api/items/", json={"name": "テストアイテム1"})
+    client.post("/api/items/", json={"name": "テストアイテム2"})
     
     # アイテムを取得して検証
-    response = client.get("/items/")
+    response = client.get("/api/items/")
     assert response.status_code == 200
     assert "テストアイテム1" in response.json()["items"]
     assert "テストアイテム2" in response.json()["items"]
